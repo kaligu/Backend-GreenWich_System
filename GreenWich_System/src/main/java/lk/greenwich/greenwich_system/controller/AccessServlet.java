6050c02ba1f65510cd12fc885ffaf96cdc3aeae7
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-07-25 04:16:34 +0530

    Start project using layerd architecture

---
+++ GreenWich_System/src/main/java/lk/greenwich/greenwich_system/controller/AccessServlet.java
@@ -0,0 +1,23 @@
+package lk.greenwich.greenwich_system.controller; /**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 7/25/2023
+ * Time    : 4:12 AM
+ */
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AccessServlet", value = "/Access-Login")
+public class AccessServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(" tested");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
